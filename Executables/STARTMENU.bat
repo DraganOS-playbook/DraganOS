6e45ec07d56c5483effa0d8faff768c9753f7de1
zipmishahl2 <110753825+zipmishahl2@users.noreply.github.com>
2024-07-21 12:35:18 +0700

    Update STARTMENU.bat

--- Executables/STARTMENU.bat
+++ Executables/STARTMENU.bat
@@ -8,7 +8,39 @@ dism.exe /online /disable-feature /featurename:MathRecognizer
 dism.exe /online /disable-feature /featurename:App.StepsRecorder
 dism.exe /Online /Cleanup-Image /StartComponentCleanup
 
-copy /y "Layout.xml" "!userAppdata!\Microsoft\Windows\Shell\LayoutModification.xml"
+mkdir "%SYSTEMDRIVE%\Users\Default\AppData\Local\Microsoft\Windows\Shell"
+copy /y "LayoutModification.xml" "%SYSTEMDRIVE%\Users\Default\AppData\Local\Microsoft\Windows\Shell\LayoutModification.xml"
+copy /y "LayoutModification.json" "%SYSTEMDRIVE%\Users\Default\AppData\Local\Microsoft\Windows\Shell\LayoutModification.json"
+copy /y "DefaultLayouts.xml" "%SYSTEMDRIVE%\Users\Default\AppData\Local\Microsoft\Windows\Shell\DefaultLayouts.xml"
+
+mkdir "%SYSTEMDRIVE%\Users\Default\AppData\Local\Packages\%%d\LocalState" 2>nul
+copy /y "settings.json" "%SYSTEMDRIVE%\Users\Default\AppData\Local\Packages\%%d\LocalState\settings.json"
+
+for /f "usebackq tokens=2 delims=\" %%a in (`reg query "HKEY_USERS" ^| findstr /r /x /c:"HKEY_USERS\\S-.*" /c:"HKEY_USERS\\AME_UserHive_[^_]*"`) do (
+	@REM If the "Volatile Environment" key exists, that means it is a proper user. Built in accounts/SIDs do not have this key.
+	reg query "HKEY_USERS\%%a" | findstr /c:"Volatile Environment" /c:"AME_UserHive_" > nul 2>&1
+	if not !errorlevel! == 1 (
+		for /f "usebackq tokens=3* delims= " %%b in (`reg query "HKU\%%a\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" /v "Local AppData" 2^>nul ^| findstr /r /x /c:".*Local AppData[ ]*REG_SZ[ ].*"`) do (
+			echo copy /y "LayoutModification.xml" "%%c\Microsoft\Windows\Shell\LayoutModification.xml"
+			copy /y "LayoutModification.xml" "%%c\Microsoft\Windows\Shell\LayoutModification.xml"
+			echo copy /y "LayoutModification.json" "%%c\Microsoft\Windows\Shell\LayoutModification.json"
+			copy /y "LayoutModification.json" "%%c\Microsoft\Windows\Shell\LayoutModification.json"
+			echo copy /y "DefaultLayouts.xml" "%%c\Microsoft\Windows\Shell\DefaultLayouts.xml"
+			copy /y "DefaultLayouts.xml" "%%c\Microsoft\Windows\Shell\DefaultLayouts.xml"
+
+			@REM Clear start menu pinned items
+			for /f "usebackq delims=" %%d in (`dir /b "%%c\Packages" /a:d ^| findstr /c:"Microsoft.Windows.StartMenuExperienceHost"`) do (
+				for /f "usebackq delims=" %%e in (`dir /b "%%c\Packages\%%d\LocalState" /a:-d ^| findstr /R /c:"start.\.bin" /c:"start\.bin"`) do (
+					echo del /q /f "%%c\Packages\%%d\LocalState\%%e"
+					del /q /f "%%c\Packages\%%d\LocalState\%%e"
+				)
+			)
+
+			for /f "usebackq delims=" %%d in (`dir /b "%%c\Packages" /a:d ^| findstr /c:"Microsoft.DesktopAppInstaller"`) do (
+				mkdir "%%c\Packages\%%d\LocalState" 2>nul
+				echo copy /y "settings.json" "%%c\Packages\%%d\LocalState\settings.json"
+				copy /y "settings.json" "%%c\Packages\%%d\LocalState\settings.json"
+			)
 
 reg delete "HKU\%%a\Software\Microsoft\Windows\CurrentVersion\Start" /v "Config" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Power\PowerSettings\54533251-82be-4824-96c1-47b60b740d00\0cc5b647-c1df-4637-891a-dec35c318583" /v "Attributes" /t reg_dword /d "0" /f
